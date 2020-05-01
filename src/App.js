3e08eaf3a3c8bd724bcc9bb20970a7c0dd5ba1b0
Emin <eminaydin93@yahoo.com>
2020-05-01 03:05:09 +0200

    material table imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import _ from 'lodash'
+import React, { useState } from 'react';
 import './App.css';
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
-import { Card, Icon, Image, Input, Button } from 'semantic-ui-react';
-
+import { Card, Icon, Image, Input, Table } from 'semantic-ui-react';
+import Moment from 'react-moment';
 
 function App() {
-
-  const [userName, setuserName] = useState("");
-  const [name, setName] = useState("");
-  const [follower, setFollower] = useState("");
-  const [following, setFollowing] = useState("");
-  const [repos, setRepos] = useState("");
-  const [avatar, setAvatar] = useState("");
   const [input, setInput] = useState("");
-  const [location, setLocation] = useState("");
   const baseUrl = `https://api.github.com/users/${input}`;
   const [userdata, setUserdata] = useState("");
+  const [column, setcolumn] = useState(null);
+  const tableData = [
+    { name: 'John', age: 15, gender: 'Male' },
+    { name: 'Amber', age: 40, gender: 'Female' },
+    { name: 'Leslie', age: 25, gender: 'Other' },
+    { name: 'Ben', age: 70, gender: 'Male' },
+  ]
+  const [data, setdata] = useState(tableData);
+  const [direction, setdirection] = useState(null);
+
+  const handleSort = (clickedColumn) => () => {
+
+    if (column !== clickedColumn) {
+
+      setcolumn(clickedColumn)
+      setdata(_.sortBy(data, [clickedColumn]))
+      setdirection('ascending')
+
+
+      return
+    }
+    setdata(data.reverse())
+    setdirection(direction === 'ascending' ? 'descending' : 'ascending')
+
+  }
+
+
 
   function search(e) {
+
     if (e.key === "Enter") {
       fetch(baseUrl)
         .then(res => res.json())
@@ -37,35 +58,76 @@ function App() {
 
   return (
     <div className="app">
-
-      <Input icon='users' iconPosition='left' placeholder='Search users...'
-        onChange={e => { setInput(e.target.value) }}
-        value={input}
-        onKeyDown={search} />
-      <Card>
+      <div className="form">
+        <Input icon='users' iconPosition='left' placeholder='Search users...'
+          onChange={e => { setInput(e.target.value) }}
+          value={input}
+          onKeyDown={search} />
+      </div>
+      <Card className="ui-card">
         <Image src={userdata.avatar_url} wrapped ui={false} />
         <Card.Content>
+          <Card.Header>{userdata.login}</Card.Header>
           <Card.Header>{userdata.location}</Card.Header>
           <Card.Meta>
-            <span className='date'>Joined in 2015</span>
+            <span className='date'> Member since: <Moment format="DD-MM-YYYY">
+              {userdata.created_at}
+            </Moment></span>
           </Card.Meta>
           <Card.Description>
-            Matthew is a musician living in Nashville.
-      </Card.Description>
+            {userdata.bio}
+          </Card.Description>
         </Card.Content>
         <Card.Content extra>
+
           <a>
             <Icon name='user' />
-            {userdata.followers}
+            {userdata.followers} Followers
           </a>
         </Card.Content>
         <Card.Content extra>
           <a>
             <Icon name='user' />
-        22 Friends
+            {userdata.followers} Following
       </a>
         </Card.Content>
       </Card>
+      <div className="table">
+
+        <Table sortable celled fixed>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={column === 'name' ? direction : null}
+                onClick={handleSort('name')}
+              >
+                Name
+            </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'age' ? direction : null}
+                onClick={handleSort('age')}
+              >
+                Age
+            </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'gender' ? direction : null}
+                onClick={handleSort('gender')}
+              >
+                Gender
+            </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+
+            <Table.Row key={""}>
+              <Table.Cell>{userdata.login}</Table.Cell>
+              <Table.Cell>{""}</Table.Cell>
+              <Table.Cell>{""}</Table.Cell>
+            </Table.Row>
+
+          </Table.Body>
+        </Table>
+      </div>
     </div>
   );
 }
