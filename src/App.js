df3947d34d32227e8fdc289f169d8cdb7f932445
Emin <eminaydin93@yahoo.com>
2020-05-05 22:44:32 +0200

    Files seperated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import _ from 'lodash'
+
 import React, { useState } from 'react';
 import './App.css';
-import { Card, Icon, Image, Input, Table } from 'semantic-ui-react';
-import Moment from 'react-moment';
+import { Card, Icon, Image, Input, Table, Grid } from 'semantic-ui-react';
+import UserTable from './components/userInfoTable/UserTable';
+import UserCard from './components/userCard/UserCard';
+
 
 function App() {
   const [input, setInput] = useState("");
   const baseUrl = `https://api.github.com/users/${input}`;
   const repoUrl = `https://api.github.com/users/${input}/repos`
-  const [userdata, setUserdata] = useState("");
+  const [userData, setUserData] = useState("");
   const [repoData, setRepoData] = useState([]);
-  const [column, setColumn] = useState(null);
-  const [data, setData] = useState("");
-  const [direction, setDirection] = useState(null);
-
-  const handleSort = (clickedColumn) => () => {
-
-    if (column !== clickedColumn) {
-
-      setColumn(clickedColumn)
-      setData(_.sortBy(repoData, [clickedColumn]))
-      setDirection('ascending')
-      return
-    }
-    setData(repoData.reverse())
-    setDirection(direction === 'ascending' ? 'descending' : 'ascending')
-
-  }
+  const [searchDone, setSearchDone] = useState(false);
 
   function search(e) {
 
     if (e.key === "Enter") {
+      setSearchDone(true)
       fetch(baseUrl)
         .then(res => res.json())
         .then(data => {
-          setUserdata(data)
+          setUserData(data)
         }
 
         );
@@ -45,93 +32,41 @@ function App() {
         )
         .then(data => {
           setRepoData(data)
-          console.log(repoData);
-
         }
-
         )
-
       setInput("")
     }
   }
+  console.log(repoData);
 
   return (
     <div className="app">
-      <div className="outer-grid">
-        <div className="form">
-          <Input icon='users' iconPosition='left' placeholder='Search users...'
-            onChange={e => { setInput(e.target.value) }}
-            value={input}
-            onKeyDown={search} />
-        </div>
-        <Card className="ui-card">
-          <Image src={userdata.avatar_url} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{userdata.login}</Card.Header>
-            <Card.Header>{userdata.location}</Card.Header>
-            <Card.Meta>
-              <span className='date'> Member since: <Moment format="DD-MM-YYYY">
-                {userdata.created_at}
-              </Moment></span>
-            </Card.Meta>
-            <Card.Description>
-              {userdata.bio}
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-
-            <a>
-              <Icon name='user' />
-              {userdata.followers} Followers
-          </a>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              {userdata.followers} Following
-      </a>
-          </Card.Content>
-        </Card>
-        <div className="table">
-
-          <Table sortable celled fixed>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell
-                  sorted={column === 'name' ? direction : null}
-                  onClick={handleSort('name')}
-                >
-                  Repository Name
-            </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'age' ? direction : null}
-                  onClick={handleSort('age')}
-                >
-                  Description
-            </Table.HeaderCell>
-                <Table.HeaderCell
-                  sorted={column === 'gender' ? direction : null}
-                  onClick={handleSort('gender')}
-                >
-                  Created At
-            </Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {repoData.map(repos => {
-                return <Table.Row key={""}>
-
-                  <Table.Cell>{repos.name}</Table.Cell>
-                  <Table.Cell>{repos.full_name}</Table.Cell>
-                  <Table.Cell>{repos.created_at}</Table.Cell>
-                </Table.Row>
-              })}
+      <Input icon='users' iconPosition='left' placeholder='Search users...'
+        onChange={e => { setInput(e.target.value) }}
+        value={input}
+        onKeyDown={search}
+        className="input-field" />
 
 
-            </Table.Body>
-          </Table>
-        </div>
-      </div>
+
+      {searchDone ?
+        <Grid celled>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <UserCard userData={userData} />
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <UserTable repoData={repoData} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+
+        : null}
+
+
+
+
     </div >
   );
 }
