2a67eee23d083a6fc2539194e4682023201ebfb0
eminaydin <eminaydin93@yahoo.com>
2020-05-08 03:15:44 +0200

    Add conditions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,52 @@
 import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react'
+import PopularCards from '../PopularCards/PopularCards';
 
 const Navbar = () => {
-    const [activeItem, setActiveItem] = useState();
+    const [activeItem, setActiveItem] = useState("");
+    const [popularRepos, setPopularRepos] = useState("");
+    const [navClicked, setNavClicked] = useState(false);
+
 
     function handleItemClick(e, { name }) {
         setActiveItem(name)
-    }
-
-    function popularRepos(e, { name }) {
-        setActiveItem(name)
-        fetch("https://api.github.com/search/repositories?q=stars:%3E1+language:Ruby&sort=stars&order=desc&type=Repositories%27")
+        fetch(`https://api.github.com/search/repositories?q=stars:%3E1+language:${name}&sort=stars&order=desc&type=Repositories%27`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                setPopularRepos(data)
+                setNavClicked(true)
+            }
+            )
+
+
+        console.log(popularRepos);
 
     }
 
     return (
-        <Menu fluid widths={3}>
-            <Menu.Item
-                name='popularRepos'
-                active={activeItem === 'popularRepos'}
-                onClick={popularRepos}
-            />
-            <Menu.Item
-                name='MostCommittedRepos'
-                active={activeItem === 'MostCommittedRepos'}
-                onClick={handleItemClick}
-            />
-            <Menu.Item
-                name='MostRecentRepos'
-                active={activeItem === 'MostRecentRepos'}
-                onClick={handleItemClick}
-            />
-        </Menu>
+        <>
+            <h2>Search for Popular Repositories</h2>
+            <Menu fluid widths={3}>
+                <Menu.Item
+                    name='Javascript'
+                    active={activeItem === 'Javascript'}
+                    onClick={handleItemClick}
+                />
+
+                <Menu.Item
+                    name='Ruby'
+                    active={activeItem === 'Ruby'}
+                    onClick={handleItemClick}
+                />
+
+                <Menu.Item
+                    name='C++'
+                    active={activeItem === 'C++'}
+                    onClick={handleItemClick}
+                />
+            </Menu>
+            {navClicked ? <PopularCards popularRepos={popularRepos} /> : null}
+        </>
     );
 }
 
