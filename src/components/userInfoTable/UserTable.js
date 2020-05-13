3a6f0188ac8bd40177973091033446a8c5f20ace
eminaydin <eminaydin93@yahoo.com>
2020-05-13 13:22:14 +0200

    Sort table

--- src/components/userInfoTable/UserTable.js
+++ src/components/userInfoTable/UserTable.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'semantic-ui-react';
 import Moment from 'react-moment';
-
+import _ from "lodash";
 
 const UserTable = (props) => {
+    const [state, setState] = useState({
+        column: null,
+        data: null,
+        direction: null,
+    });
+    const { column, data, direction } = state;
+    const handleSort = (clickedColumn) => () => {
+        if (column !== clickedColumn) {
+            setState({
+                data: _.sortBy(data, [clickedColumn]),
+                column: clickedColumn,
+                direction: "ascending",
+            });
+            return;
+        }
+        setState({
+            data: props.repoData.reverse(),
+            column: clickedColumn,
+            direction: direction === "ascending" ? "descending" : "ascending",
+        });
 
+    };
     return (
         <div>
-            <Table sortable celled fixed selectable>
+            <Table sortable celled fixed>
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell
-
+                            sorted={column === 'name' ? direction : null}
+                            onClick={handleSort('name')}
                         >
                             Repository Name
-            </Table.HeaderCell>
+        </Table.HeaderCell>
                         <Table.HeaderCell
+                            sorted={column === 'description' ? direction : null}
+                            onClick={handleSort('description')}
                         >
                             Description
-            </Table.HeaderCell>
+        </Table.HeaderCell>
                         <Table.HeaderCell
-
+                            sorted={column === 'since' ? direction : null}
+                            onClick={handleSort('since')}
                         >
                             Created At
-            </Table.HeaderCell>
+        </Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
@@ -39,7 +64,6 @@ const UserTable = (props) => {
 
                 </Table.Body>
             </Table>
-
         </div>
     );
 }
