11d9851ce84602b0b30a1bf07e90a504d872ccfe
eminaydin <eminaydin93@yahoo.com>
2020-05-13 13:22:53 +0200

    Card Details

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -1,38 +1,22 @@
 import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { List, Image } from 'semantic-ui-react';
 import Moment from 'react-moment';
 
 const UserCard = (props) => {
 
     return (
-        <Card className="ui-card">
-            <Image src={props.userData.avatar_url} wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{props.userData.login}</Card.Header>
-                <Card.Meta>
-                    Member since: <Moment format="DD-MM-YYYY">
-                        {props.userData.created_at}
-                    </Moment>
-                </Card.Meta>
-                <Card.Description>
-                    {props.userData.bio}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-
-                <a href=" ">
-                    <Icon name='user' />
-                    {props.userData.followers} Followers
-  </a>
-            </Card.Content>
-            <Card.Content extra>
-                <a href=" ">
-                    <Icon name='user' />
-                    {props.userData.followers} Following
-</a>
-            </Card.Content>
-
-        </Card>
+        <div className="user-card">
+            <Image src={props.userData.avatar_url} size="medium" />
+            <List divided style={{ margin: "auto" }}>
+                <h2>{props.userData.login}</h2>
+                <List.Item icon='users' content={`${props.userData.followers} Followers`} />
+                <List.Item icon='users' content={`${props.userData.following} Following`} />
+                <List.Item icon='marker' content={`${props.userData.location}`} />
+                <List.Item icon='users' content={`Member since: 
+                    ${<Moment format="DD-MM-YYYY"> props.userData.created_at </Moment>}
+                    `} />
+            </List>
+        </div>
 
     );
 }
