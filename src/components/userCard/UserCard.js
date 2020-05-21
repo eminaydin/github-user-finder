151fd8c501cd19f34e8cbb9459449568a0e328e6
eminaydin <eminaydin93@yahoo.com>
2020-05-21 14:19:20 +0200

    Destructure

--- src/components/userCard/UserCard.js
+++ src/components/userCard/UserCard.js
@@ -2,35 +2,35 @@ import React from 'react';
 import { Card, Image, Icon } from 'semantic-ui-react';
 import Moment from 'react-moment';
 
-const UserCard = (props) => {
-    console.log(props.userData);
+const UserCard = ({ userData }) => {
+
 
     return (
         <div className="user-card">
             <Card centered>
-                <Image src={props.userData.avatar_url} size="medium" wrapped ui={false} />
+                <Image src={userData.avatar_url} size="medium" wrapped ui={false} />
                 <Card.Content extra>
                     <a>
                         <Icon name='user circle' />
-                        {props.userData.login}
+                        {userData.login}
                     </a>
                 </Card.Content>
                 <Card.Content extra>
                     <a>
                         <Icon name='user' />
-                        {props.userData.followers} Followers
+                        {userData.followers} Followers
                     </a>
                 </Card.Content>
                 <Card.Content extra>
                     <a>
                         <Icon name='user secret' />
-                        {props.userData.following} Following
+                        {userData.following} Following
                     </a>
                 </Card.Content>
                 <Card.Content extra>
                     <a>
                         <Icon name='time' />
-                        <Moment format="DD-MM-YYYY"> {props.userData.created_at} </Moment>
+                        <Moment format="DD-MM-YYYY"> {userData.created_at} </Moment>
                     </a>
                 </Card.Content>
             </Card>
